eae1bb0768e3f195bc03d2e04ed31c066b216e5c
Umesh M <umeshm.m@gmail.com>
2023-09-21 16:44:19 +0530

    added sop

--- Demo.java
+++ Demo.java
@@ -2,6 +2,8 @@ class Demo
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!");
+		System.out.println("hi guys");
+			System.out.println("hi guys");
+				System.out.println("hi guys");
 	}
 }
